36bedf1844eec4c6b03ccc8a862b60a0d04463b9
Jawali Vooturi <jawalivooturi@gmail.com>
2024-03-19 15:42:06 +0530

    Submitting the code.

---
+++ src/main/java/Program.java
@@ -0,0 +1,11 @@
+package com.qualizeal.javaapp.main;
+	
+import com.qualizeal.javaapp.mathematics.Subtraction;
+
+public class Program{
+public static void main(String[] args) {
+
+	Subtraction subtraction = new Subtraction();
+	subtraction.subtract();
+}
+}
---
+++ src/main/java/Subtraction.java
@@ -0,0 +1,15 @@
+package com.qualizeal.javaapp.mathematics;
+
+public class Subtraction {
+	
+	public void subtract() {
+	
+  	int a = 10;
+	
+	int b = 5;
+
+	int r = a - b;
+		
+	System.out.println(r);
+	}
+}
